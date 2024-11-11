5316329b3c666510278f0ac4ea21a073b6064609
suzanaILIEVA <suzanaa.ilieva@gmail.com>
2024-11-12 01:44:14 +0300

    project created

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{backgroundColor: '#fff', flex: 1}}>
+      <Text>Apppppp</Text>
+    </View>
+  );
+};
+
+export default App;
